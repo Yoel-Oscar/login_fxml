b5001e0c17d19bdbc3228d62fb7f187f2a41b12a
Robby Tan <robby-tan@outlook.com>
2021-10-22 23:25:21 +0700

    Change query to select id, name, and email. Change code in while loop to match model

--- src/com/yoel/dao/UserDaoImplementation.java
+++ src/com/yoel/dao/UserDaoImplementation.java
@@ -1,40 +1,36 @@
 package com.yoel.dao;
 
-/*
-  author 1672063 Yoel Oscar
- */
-
-
 import com.yoel.entity.User;
 import com.yoel.util.LoginServices;
 import com.yoel.util.MySQLConnection;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**
+ * @author 1672063 Yoel Oscar
+ */
 
-
-
-public class UserDaoImplementation implements LoginServices<User> {
-
-
+public class UserDaoImplementation implements LoginServices {
     @Override
     public User login(String email, String password) throws ClassNotFoundException, SQLException {
-        User usersData = null;
+        User user = null;
         try (Connection connection = MySQLConnection.createConnection()) {
-            String query = "SELECT email, password FROM user WHERE email =? AND password = MD5(?)";
+            String query = "SELECT id, name, email FROM user WHERE email =? AND password = MD5(?)";
             try (PreparedStatement pstmt = connection.prepareStatement(query)) {
                 pstmt.setString(1, email);
                 pstmt.setString(2, password);
-                try (ResultSet rsltset = pstmt.executeQuery()) {
-                    while (rsltset.next()) {
-                        usersData.setEmail(rsltset.getString("email"));
-                        usersData.setPassword(rsltset.getString("password"));
+                try (ResultSet rsts = pstmt.executeQuery()) {
+                    while (rsts.next()) {
+                        user.setId(rsts.getInt("id"));
+                        user.setName(rsts.getString("name"));
+                        user.setEmail(rsts.getString("email"));
                     }
                 }
             }
         }
-        return usersData;
+        return user;
     }
 }
